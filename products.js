6d9553b4d2cbe3851d61b2b87a1dbba75b1424c6
sonjoyjr10 <snbinance40@gmail.com>
2026-07-20 00:30:24 +0600

    Update products.js

--- products.js
+++ products.js
@@ -10,16 +10,16 @@ const products = [
   },
   {
     id: 2,
-    name: "Product Two",
-    price: 2200,
+    name: "Air pods Gen 3",
+    price: 1900,
     image: "https://i.ibb.co.com/SwxyR23n/file-00000000c7cc820b80509c4c25b0e3a7.png",
-    category: "Fashion",
+    category: "Gadget",
     trending: false
   },
   {
     id: 3,
-    name: "Product Three",
-    price: 999,
+    name: "apple air pods",
+    price: 28000,
     image: "https://i.ibb.co.com/SwxyR23n/file-00000000c7cc820b80509c4c25b0e3a7.png",
     category: "Electronics",
     trending: true
