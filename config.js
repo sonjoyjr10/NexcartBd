4c0f2af7046d65ddb3aecd23fa25549d51b686cd
sonjoyjr10 <snbinance40@gmail.com>
2026-07-20 20:39:59 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // NEXCARTBD — API configuration
 // Replace API_URL with your real backend base URL.
 
-const API_URL = "https://api.nexcartbd.com";
+const API_URL = "https://script.google.com/macros/s/AKfycbwSrInmFfHanLBvaLSsBx5swYf-Fy5eau4XTKmmHP3iqz_47hHzs32V0KHe2ZKlGl5Q/exec";
 
 const API_ACTIONS = {
   REGISTER: `${API_URL}/auth/register`,
